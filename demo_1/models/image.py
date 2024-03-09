6e57578d6c1f39e9581c476190703143db11cf6c
VishalVaghela3969 <vishalvaghela3969@gmail.com>
2024-03-09 13:57:16 +0530

    seq

--- demo_1/models/image.py
+++ demo_1/models/image.py
@@ -6,6 +6,7 @@ class Image(models.Model):
     _description = 'Product or Business Image'
     _rec_name = 'product_id'
 
+
     business_id = fields.Many2one('business', string='Business')
     product_id = fields.Many2one('product', string='Product')
     image_url = fields.Char(string='Image URL')
